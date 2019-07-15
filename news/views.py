0f39e3d51cbc4b4d67cd98872a3a90b0b6a2db32
williammango <williammango2015@gmail.com>
2019-07-15 15:27:17 +0300

    extend base.html and create navbar

--- news/views.py
+++ news/views.py
@@ -11,7 +11,6 @@ def welcome(request):
 def news_of_day(request):
     date = dt.date.today()
     # function to convert date object to find exact day
-    day = convert_dates(date)
 
     return render(request, 'all-news/today-news.html',{'date':date,})
 def past_days_news(request,past_date):
