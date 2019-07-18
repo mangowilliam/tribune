b0125e307da4e35d8ed6564ee10df985aae626d4
williammango <williammango2015@gmail.com>
2019-07-18 10:33:05 +0300

    add search form

--- news/tests.py
+++ news/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Editor, Article, tags
+import datetime as dt
 
 # Create your tests here.
 class EditorTestclass(TestCase):
@@ -10,7 +11,37 @@ class EditorTestclass(TestCase):
         self.assertTrue(isinstance(self.kimberly,Editor))
     
     def test_save(self):
-        self.kimberly.save_Editor()
+        self.kimberly.save_editor()
         editors = Editor.objects.all()
         self.assertTrue(len(editors)>0)
         
+        
+class ArticleTestClass(TestCase):
+    
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.kimberly = Editor(first_name = 'kimberly',last_name = 'kardashian', email='kimkay@gmail.com')
+        self.kimberly.save_editor()
+        # Creating a new tag and saving it
+        self.new_tag = tags(name = 'testing')
+        self.new_tag.save()
+
+        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.kimberly)
+        self.new_article.save()
+
+        self.new_article.tags.add(self.new_tag)
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        tags.objects.all().delete()
+        Article.objects.all().delete()
+        
+    def test_get_news_today(self):
+        today_news = Article.todays_news()
+        self.assertTrue(len(today_news)>0)
+        
+    def test_get_news_by_date(self):
+        test_date = '2017-03-17'
+        date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
+        news_by_date = Article.days_news(date)
+        self.assertTrue(len(news_by_date) == 0)
--- news/views.py
+++ news/views.py
@@ -1,18 +1,17 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render,redirect
 import datetime as dt
+from .models import Article
 
 
 # Create your views here.
-def welcome(request):
 
-    return render(request, 'welcome.html')
-
-def news_of_day(request):
+def news_today(request):
     date = dt.date.today()
+    news = Article.todays_news()
     # function to convert date object to find exact day
 
-    return render(request, 'all-news/today-news.html',{'date':date,})
+    return render(request, 'all-news/today-news.html',{'date':date,'news':news})
 def past_days_news(request,past_date):
     try:
         
@@ -25,4 +24,25 @@ def past_days_news(request,past_date):
     
     if date == dt.date.today():
         return redirect(new_of_day)
-    return render(request, 'all-news/past-news.html',{'date':date})
+    
+    news = Article.days_news(date)
+    return render(request, 'all-news/past-news.html',{'date':date,"news":news})
+def search_results(request):
+    
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = Article.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-news/search.html',{"message":message})
+    
+def article(request,article_id):
+    try:
+        article = Article.objects.get(id = article_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"all-news/article.html", {"article":article})
